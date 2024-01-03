b3f86058660123fb137ea0b1db8c03445b29c76e
Gavrilencu <115806851+Gavrilencu@users.noreply.github.com>
2024-01-03 11:10:22 +0200

    Update UserResponse.cs

--- Models/UserResponse.cs
+++ Models/UserResponse.cs
@@ -1,12 +1,16 @@
-﻿namespace feedback.Models
+namespace feedback.Models
 {
     public class UserResponse
     {
-        public int Id { get; set; }
-        public int QuestionId { get; set; }
-        public int IdChestionar { get; set; } // ID-ul chestionarului
-        public string Raspuns { get; set; }
-        public string NumePersoana { get; set; } // Opțional, folosit doar pentru răspunsurile de tip feedback
+        public int? Id { get; set; }
+        public int? QuestionId { get; set; }
+        public int? IdChestionar { get; set; } // ID-ul chestionarului
+        public string? Raspuns { get; set; }
+        public string? ForPerson { get; set; } // Opțional, folosit doar pentru răspunsurile de tip feedback
+        public string? FromPerson { get; set; }
+        public string? EncodedName { get; set; }
+        public string? PostDate { get; set; }
+
     }
 
 
